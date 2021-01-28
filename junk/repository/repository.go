efb970abcaf3eb2770f06bb8995316df8f1a830a
Kamil <unnamedxaer@gmail.com>
2021-01-28 22:38:25 +0100

    Remove junk code

--- junk/repository/repository.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "github.com/unnamedxaer/gymm-api/server/models"
-
-type IRepository interface {
-	Initialize(uri string) error
-	CreateUser(u *models.User) error
-	GetUserById(u *models.User) error
-}
