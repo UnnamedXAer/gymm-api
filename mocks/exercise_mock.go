1bc50dc54c402a5b3563ef95b30efeebb9177801
Kamil <unnamedxaer@gmail.com>
2021-04-12 20:01:43 +0200

    Add mocks for exercise

--- mocks/exercise_mock.go
+++ mocks/exercise_mock.go
@@ -1,16 +1,70 @@
 package mocks
 
 import (
+	"time"
+
 	"github.com/unnamedxaer/gymm-api/entities"
+	"github.com/unnamedxaer/gymm-api/repositories"
 	"github.com/unnamedxaer/gymm-api/usecases"
 )
+
+var ExampleExercise = &entities.Exercise{
+	ID:          "6072d3206144644984a54fa0",
+	Name:        "Deadlift",
+	Description: "The deadlift is a weight training exercise in which a loaded barbell or bar is lifted off the ground to the level of the hips, torso perpendicular to the floor, before being placed back on the ground. It is one of the three powerlifting exercises, along with the squat and bench press.",
+	SetUnit:     entities.Weight,
+	CreatedAt:   time.Now().UTC(),
+	CreatedBy:   "607000e83db4e97a55229bf2",
+}
 
 func InsertMockExercise(er usecases.ExerciseRepo) (*entities.Exercise, error) {
 
 	return er.CreateExercise(
-		"Deadlift",
-		"The deadlift is a weight training exercise in which a loaded barbell or bar is lifted off the ground to the level of the hips, torso perpendicular to the floor, before being placed back on the ground. It is one of the three powerlifting exercises, along with the squat and bench press.",
-		entities.Weight,
-		"607000e83db4e97a55229bf2",
+		ExampleExercise.Name,
+		ExampleExercise.Description,
+		ExampleExercise.SetUnit,
+		ExampleExercise.CreatedBy,
 	)
+}
+
+type MockExerciseRepo struct{}
+
+func (er *MockExerciseRepo) CreateExercise(name, description string, setUnit entities.SetUnit, createdBy string) (*entities.Exercise, error) {
+
+	return &entities.Exercise{
+		ID:          ExampleExercise.ID,
+		Name:        name,
+		Description: description,
+		SetUnit:     setUnit,
+		CreatedAt:   ExampleExercise.CreatedAt,
+		CreatedBy:   createdBy,
+	}, nil
+}
+
+func (er *MockExerciseRepo) GetExerciseByID(id string) (*entities.Exercise, error) {
+	if ExampleExercise.ID == id {
+		return ExampleExercise, nil
+	}
+
+	return nil, repositories.NewErrorNotFoundRecord()
+}
+
+func (er *MockExerciseRepo) UpdateExercise(ex *entities.Exercise) (*entities.Exercise, error) {
+	if ex.ID == "" {
+		return nil, repositories.NewErrorInvalidID(ex.ID)
+	}
+
+	out := *ExampleExercise
+	out.ID = ex.ID
+	if ex.Description != "" {
+		out.Description = ex.Description
+	}
+	if ex.Name != "" {
+		out.Name = ex.Name
+	}
+	if ex.SetUnit != 0 {
+		out.SetUnit = ex.SetUnit
+	}
+
+	return &out, nil
 }
