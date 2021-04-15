90edc4800d9088d1f5e18e1b4684be10c776c2bd
UnnamedXAer <UnnamedXAer@gmail.com>
2021-04-15 14:19:27 +0200

    update exercise validation
    adds validation for exercise update
    refactors validation of new exercise
    refactor tests for new exercise
    adds  tests for exercise update validation

--- endpoints/http/http_exercise_validation.go
+++ endpoints/http/http_exercise_validation.go
@@ -21,45 +21,130 @@ func validateExerciseInput(validate *validator.Validate, exercise *usecases.Exer
 	}
 
 	formattedErrors := make(map[string]string, len(validateErrs))
-	var errText, txt string
 	for _, err := range validateErrs {
 		fieldName := err.Field()
 		fn, ok := validation.GetFieldJSONTag(exercise, fieldName)
 		if ok {
 			fieldName = fn
 		}
-		switch err.Tag() {
-		case "set_unit":
-			txt = fmt.Sprintf("The '%s' is incorrect, allowed values: 1 - 'weight', 2 - 'time'. ", fieldName)
-		case "required":
-			txt = fmt.Sprintf("The '%s' field value is required and cannot be empty. ", fieldName)
-		case "min":
-			var objLengthUnit string
-			if err.Kind() == reflect.String {
-				objLengthUnit = "characters"
-			} else {
-				objLengthUnit = "elements"
-			}
-
-			txt = fmt.Sprintf("The '%s' has to be at least %s %s long. ", fieldName, err.Param(), objLengthUnit)
-		case "max":
-			var objLengthUnit string
-			if err.Kind() == reflect.String {
-				objLengthUnit = "characters"
-			} else {
-				objLengthUnit = "elements"
-			}
-
-			txt = fmt.Sprintf("The '%s' has to be at max %s %s long. ", fieldName, err.Param(), objLengthUnit)
-		case "ex_name_chars":
-			txt = fmt.Sprintf("The '%s' is incorrect, allowed are: letters and numbers. ", fieldName)
-		case "printascii":
-			txt = fmt.Sprintf("The '%s' is incorrect, allowed are: printable characters. ", fieldName)
-		default:
-			txt = fmt.Sprintf("The '%s' field failed on the '%s' tag validation. ", fieldName, err.Tag())
-		}
-		errText += txt
-		formattedErrors[fieldName] = txt
+		formattedErrors[fieldName] += getErrorTranslation(&err, fieldName)
 	}
-	return validation.NewStructValidError(formattedErrors, errText)
+	if len(formattedErrors) > 0 {
+		errText := concatErrors(formattedErrors)
+		return validation.NewStructValidError(formattedErrors, errText)
+	}
+	return nil
+}
+
+func validateExerciseInput4Update(validate *validator.Validate, exercise *usecases.ExerciseInput) error {
+	formattedErrors := make(map[string]string)
+	v := reflect.ValueOf(exercise).Elem()
+	for _, fieldName := range []string{"Name", "Description", "SetUnit"} {
+		validateExerciseField(validate, &v, exercise, fieldName, formattedErrors)
+	}
+
+	if len(formattedErrors) > 0 {
+		errText := concatErrors(formattedErrors)
+		return validation.NewStructValidError(formattedErrors, errText)
+	}
+	return nil
+}
+
+func validateExerciseField(
+	validate *validator.Validate,
+	v *reflect.Value,
+	exercise *usecases.ExerciseInput,
+	fieldName string,
+	formattedErrors map[string]string) {
+
+	strFld, ok := v.Type().FieldByName(fieldName)
+	if !ok {
+		formattedErrors["more"] = fmt.Sprintf("Could not parse '%s' field.", fieldName)
+		return
+	}
+
+	strFldVal := v.FieldByName(fieldName)
+	if strFldVal.IsZero() {
+		return
+	}
+
+	tagVal := strFld.Tag.Get("validate")
+	var val interface{}
+
+	switch strFldVal.Kind() {
+	case reflect.String:
+		val = strFldVal.String()
+	case reflect.Int8:
+		val = strFldVal.Int()
+	default:
+		_, ok := formattedErrors["more"]
+		if ok {
+			formattedErrors["more"] += "\n"
+		}
+		formattedErrors["more"] += fmt.Sprintf("Could not parse '%s' field.", fieldName)
+		return
+	}
+
+	err := validate.Var(val, tagVal)
+	if err != nil {
+		fn, ok := validation.GetFieldJSONTag(exercise, fieldName)
+		if ok {
+			fieldName = fn
+		}
+		translateValidationErrs(fieldName, err, formattedErrors)
+	}
+}
+
+func translateValidationErrs(fieldName string, errs error, formattedErrors map[string]string) {
+	validateErrs, ok := errs.(validator.ValidationErrors)
+	if !ok {
+		// @todo: improve error text
+		formattedErrors[fieldName] = errs.Error()
+	}
+
+	for _, err := range validateErrs {
+		formattedErrors[fieldName] += getErrorTranslation(&err, fieldName)
+	}
+}
+
+func getErrorTranslation(err *validator.FieldError, fieldName string) string {
+	switch (*err).Tag() {
+	case "set_unit":
+		return fmt.Sprintf("The '%s' is incorrect, allowed values: 1 - 'weight', 2 - 'time'. ", fieldName)
+	case "required":
+		return fmt.Sprintf("The '%s' field value is required and cannot be empty. ", fieldName)
+	case "min":
+		var objLengthUnit string
+		if (*err).Kind() == reflect.String {
+			objLengthUnit = "characters"
+		} else {
+			objLengthUnit = "elements"
+		}
+
+		return fmt.Sprintf("The '%s' has to be at least %s %s long. ", fieldName, (*err).Param(), objLengthUnit)
+	case "max":
+		var objLengthUnit string
+		if (*err).Kind() == reflect.String {
+			objLengthUnit = "characters"
+		} else {
+			objLengthUnit = "elements"
+		}
+
+		return fmt.Sprintf("The '%s' has to be at max %s %s long. ", fieldName, (*err).Param(), objLengthUnit)
+	case "ex_name_chars":
+		return fmt.Sprintf("The '%s' is incorrect, allowed are: letters and numbers. ", fieldName)
+	case "printascii":
+		return fmt.Sprintf("The '%s' is incorrect, allowed are: printable characters. ", fieldName)
+	default:
+		return fmt.Sprintf("The '%s' field failed on the '%s' tag validation. ", fieldName, (*err).Tag())
+	}
+}
+
+func concatErrors(formattedErrors map[string]string) string {
+	var s string
+	for _, v := range formattedErrors {
+		s += v
+	}
+
+	return s
 }
--- endpoints/http/http_exercise_validation_test.go
+++ endpoints/http/http_exercise_validation_test.go
@@ -1,12 +1,12 @@
 package http
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/unnamedxaer/gymm-api/mocks"
 	"github.com/unnamedxaer/gymm-api/usecases"
+	"github.com/unnamedxaer/gymm-api/validation"
 )
 
 func TestValidateExerciseInput(t *testing.T) {
@@ -15,68 +15,68 @@ func TestValidateExerciseInput(t *testing.T) {
 		input *usecases.ExerciseInput
 		want  []string
 	}{
-		// {
-		// 	desc: "valid exercise input",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        mocks.ExampleExercise.Name,
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: nil,
-		// },
-		// {
-		// 	desc: "valid exercise input 2",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        mocks.ExampleExercise.Name + "2",
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: nil,
-		// },
-		// {
-		// 	desc: "valid exercise, two words name",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        "Front Squat",
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: nil,
-		// },
-		// {
-		// 	desc: "too long name",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        "too long name too long name too long name too long name",
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: []string{"'name'"},
-		// },
-		// {
-		// 	desc: "too short name",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        "t",
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: []string{"'name'"},
-		// },
-		// {
-		// 	desc: "name contains forbidden characters",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name:        "DL ╤",
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: []string{"'name'"},
-		// },
-		// {
-		// 	desc: "missing name",
-		// 	input: &usecases.ExerciseInput{
-		// 		Description: mocks.ExampleExercise.Description,
-		// 		SetUnit:     mocks.ExampleExercise.SetUnit,
-		// 	},
-		// 	want: []string{"'name'"},
-		// },
+		{
+			desc: "valid exercise input",
+			input: &usecases.ExerciseInput{
+				Name:        mocks.ExampleExercise.Name,
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: nil,
+		},
+		{
+			desc: "valid exercise input 2",
+			input: &usecases.ExerciseInput{
+				Name:        mocks.ExampleExercise.Name + "2",
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: nil,
+		},
+		{
+			desc: "valid exercise, two words name",
+			input: &usecases.ExerciseInput{
+				Name:        "Front Squat",
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: nil,
+		},
+		{
+			desc: "too long name",
+			input: &usecases.ExerciseInput{
+				Name:        "too long name too long name too long name too long name",
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: []string{"name"},
+		},
+		{
+			desc: "too short name",
+			input: &usecases.ExerciseInput{
+				Name:        "t",
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: []string{"name"},
+		},
+		{
+			desc: "name contains forbidden characters",
+			input: &usecases.ExerciseInput{
+				Name:        "DL ╤",
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: []string{"name"},
+		},
+		{
+			desc: "missing name",
+			input: &usecases.ExerciseInput{
+				Description: mocks.ExampleExercise.Description,
+				SetUnit:     mocks.ExampleExercise.SetUnit,
+			},
+			want: []string{"name"},
+		},
 		{
 			desc: "not printable chars in description: 💤",
 			input: &usecases.ExerciseInput{
@@ -84,7 +84,7 @@ func TestValidateExerciseInput(t *testing.T) {
 				Description: mocks.ExampleExercise.Description + "💤",
 				SetUnit:     mocks.ExampleExercise.SetUnit,
 			},
-			want: []string{"'description'"},
+			want: []string{"description"},
 		},
 		{
 			desc: "not printable chars in description 2: ╨",
@@ -93,7 +93,7 @@ func TestValidateExerciseInput(t *testing.T) {
 				Description: mocks.ExampleExercise.Description + "╨",
 				SetUnit:     mocks.ExampleExercise.SetUnit,
 			},
-			want: []string{"'description'"},
+			want: []string{"description"},
 		},
 		{
 			desc: "not printable chars in description 3: \t(tab)",
@@ -102,7 +102,7 @@ func TestValidateExerciseInput(t *testing.T) {
 				Description: mocks.ExampleExercise.Description + "\t 2223",
 				SetUnit:     mocks.ExampleExercise.SetUnit,
 			},
-			want: []string{"'description'"},
+			want: []string{"description"},
 		},
 		{
 			desc: "not printable chars in description 4: t",
@@ -113,24 +113,77 @@ func TestValidateExerciseInput(t *testing.T) {
 			},
 			want: nil,
 		},
-		// {
-		// 	desc: "missing description and set unit",
-		// 	input: &usecases.ExerciseInput{
-		// 		Name: mocks.ExampleExercise.Name,
-		// 	},
-		// 	want: []string{"'description'", "'setUnit'"},
-		// },
-		// {
-		// 	desc: "missing name, description and incorrect set unit",
-		// 	input: &usecases.ExerciseInput{
-		// 		SetUnit: 12,
-		// 	},
-		// 	want: []string{"'description'", "'setUnit'", "'name'"},
-		// },
+		{
+			desc: "missing description and set unit",
+			input: &usecases.ExerciseInput{
+				Name: mocks.ExampleExercise.Name,
+			},
+			want: []string{"description", "setUnit"},
+		},
+		{
+			desc: "missing name, description and incorrect set unit",
+			input: &usecases.ExerciseInput{
+				SetUnit: 12,
+			},
+			want: []string{"description", "setUnit", "name"},
+		},
 	}
+	runExerciseTestCases(t, validateExerciseInput, testCases)
+}
+
+func TestValidateExerciseInput4Update(t *testing.T) {
+	testCases := []struct {
+		desc  string
+		input *usecases.ExerciseInput
+		want  []string
+	}{
+		{
+			desc: "valid, only name",
+			input: &usecases.ExerciseInput{
+				Name: mocks.ExampleExercise.Name,
+			},
+			want: nil,
+		},
+		{
+			desc: "incorrect name, description, set unit",
+			input: &usecases.ExerciseInput{
+				Name:        mocks.ExampleExercise.Name + " 🛑 ",
+				Description: mocks.ExampleExercise.Description + " 🛑 ",
+				SetUnit:     mocks.ExampleExercise.SetUnit + 12,
+			},
+			want: []string{"name", "description", "setUnit"},
+		},
+		{
+			desc: "incorrect name, valid set unit",
+			input: &usecases.ExerciseInput{
+				Name:    "X",
+				SetUnit: mocks.ExampleExercise.SetUnit,
+			},
+			want: []string{"name"},
+		},
+		{
+			desc: "valid, only description",
+			input: &usecases.ExerciseInput{
+				Description: "The Overhead Press is a full body, compound exercise.",
+			},
+			want: nil,
+		},
+	}
+	runExerciseTestCases(t, validateExerciseInput4Update, testCases)
+}
+
+func runExerciseTestCases(
+	t *testing.T,
+	testedFunc func(validate *validator.Validate, exercise *usecases.ExerciseInput) error,
+	testCases []struct {
+		desc  string
+		input *usecases.ExerciseInput
+		want  []string
+	}) {
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			err := validateExerciseInput(validate, tC.input)
+			err := testedFunc(validate, tC.input)
+
 			if tC.want == nil {
 				if err != nil {
 					t.Errorf("want nil error, got %v, for %+v", err, tC.input)
@@ -138,14 +191,31 @@ func TestValidateExerciseInput(t *testing.T) {
 				return
 			}
 
-			errText := err.Error()
-			for _, w := range tC.want {
-				if !strings.Contains(errText, w) {
-					if err == nil {
-						fmt.Println("want not nil error")
-					}
-					t.Errorf("want %q, got %v, for %+v", w, err, tC.input)
+			strValErr, ok := err.(*validation.StructValidError)
+			if !ok {
+				t.Errorf("want errors for fields %v, got %v, for %+v", tC.want, err, tC.input)
+				return
+			}
+
+			vErrs := strValErr.ValidationErrors()
+			if len(vErrs) != len(tC.want) {
+				t.Errorf("want errors (%d) for fields %v, got (%d) %v, for %+v",
+					len(tC.want), tC.want, len(vErrs), vErrs, tC.input)
+				return
+			}
+
+			var failed bool
+			for _, f := range tC.want {
+				_, ok = vErrs[f]
+				if !ok {
+					t.Errorf("missing error for %q", f)
+					failed = true
 				}
+			}
+
+			if failed {
+				t.Logf("errors: %+v", vErrs)
+				t.Logf("input: %+v", tC.input)
 			}
 		})
 	}
