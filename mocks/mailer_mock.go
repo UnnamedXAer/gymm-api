957340b91cc239f8dd8684fc1a240d7181019368
Kamil <unnamedxaer@gmail.com>
2021-05-11 18:29:51 +0200

    fix mailer mock

--- mocks/mailer_mock.go
+++ mocks/mailer_mock.go
@@ -2,7 +2,7 @@ package mocks
 
 type MockMailer struct{}
 
-func (m *MockMailer) Send(recipients []string, data []byte) {
+func (m *MockMailer) Send(recipients []string, subject, data []byte) {
 }
 
 func (m *MockMailer) Close() {
