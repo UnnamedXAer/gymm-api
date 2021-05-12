bcd213ab1966ecf8fc19334b5b335adb91bed158
UnnamedXAer <UnnamedXAer@gmail.com>
2021-05-12 15:10:01 +0200

    fix path to template + update errors

--- usecases/email.go
+++ usecases/email.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"text/template"
 
+	"github.com/pkg/errors"
 	"github.com/unnamedxaer/gymm-api/entities"
 )
 
@@ -18,15 +19,16 @@ func generatePwdResetEmailContent(
 	user *entities.User,
 	pwdResetReq *entities.ResetPwdReq) ([]byte, error) {
 
-	tmpl, err := template.ParseFiles("../templates/templatefiles/resetpwd.html")
+	tmpl, err := template.ParseFiles("./templates/templatefiles/resetpwd.html")
 	if err != nil {
-		return nil, err
+		return nil, errors.WithMessagef(err, "generate pwd reset email content:")
 	}
 
 	clientURL := os.Getenv("CLIENT_URL")
 	appName := "The Gymm Api"
 
 	url := fmt.Sprintf("%s/password/reset/%s", clientURL, pwdResetReq.ID)
+	fmt.Println("URL:", url)
 
 	data := map[string]interface{}{
 		"User":    user,
@@ -37,7 +39,7 @@ func generatePwdResetEmailContent(
 	b := bytes.Buffer{}
 	err = tmpl.Execute(&b, &data)
 	if err != nil {
-		return nil, err
+		return nil, errors.WithMessagef(err, "generate pwd reset email content:")
 	}
 	return b.Bytes(), nil
 }
